c8e4e1576a19d6b4d0d7494393c6efeda31ac07a
igorhgds <61789282+igorhgds@users.noreply.github.com>
2024-09-02 15:34:20 -0300

    Initial commit

---
+++ 16_JAVASCRIPT_MODERNO/scripts.js
@@ -0,0 +1,200 @@
+// 1 - var, let e const
+var x = 10
+var y = 15
+
+if (y > 10){
+    var x = 5
+    console.log(x)
+}
+console.log(x)
+
+let a = 10
+let b = 15
+
+if (b > 10){
+    let a = 5
+    console.log(a)
+}
+console.log(a)
+
+function logName(){
+    const name = 'Matheus'
+    console.log(name)
+}
+
+const name = 'Igor'
+logName()
+console.log(name)
+//--------------------------------------------------------------------------->
+// 2 - Arrow Function
+const sum = function sum(a, b) {
+    return a + b
+}
+
+const arrowSum = (a, b) => {
+    return a + b
+}
+
+console.log(sum(5, 5))
+console.log(arrowSum(5, 5))
+
+const greeting = (name) => {
+    if (name) {
+        return 'Olá' + name + '!'
+    }else{
+        return 'Olá!'
+    }
+}
+
+console.log(greeting('Igor'))
+console.log(greeting())
+
+const user = {
+    name: "Theo",
+    sayUserName(){
+        var self = this
+        setTimeout(function() {
+            console.log(self)
+            console.log("Username: " + self.name)
+        }, 500)
+    },
+    sayUserNameArrow() {
+        setTimeout(() => {
+            console.log(this)
+            console.log('Username: ' + this.name)
+        }, 700)
+    }
+}
+
+//user.sayUserName()
+//user.sayUserNameArrow()
+
+//--------------------------------------------------------------------------->
+// 3 - Filter
+const arr = [1, 2, 3, 4, 5]
+
+const highNumbers = arr.filter((n) => {
+    if (n >= 3) {
+        return n
+    }
+})
+
+console.log(highNumbers)
+
+
+const users = [
+    {name: 'Igor', available: true},
+    {name: 'Lucas', available: false},
+    {name: 'Caio', available: false},
+    {name: 'Aline', available: true},
+]
+
+const availableUsers = users.filter((user) => user.available)
+
+console.log(availableUsers)
+
+//--------------------------------------------------------------------------->
+// 4 - Map
+const products = [
+    {name: 'Camisa', price: 10.99, category: 'Roupas'},
+    {name: 'Chaleira Elétrica', price: 49.99, category: 'Eletro'},
+    {name: 'Fogão', price: 400, category: 'Eletro'},
+    {name: 'Calça Jeans', price: 50.99, category: 'Roupas'},
+]
+
+products.map((product) => {
+    if(product.category === 'Roupas') {
+        product.onSale = true
+    }
+})
+
+console.log(products)
+//--------------------------------------------------------------------------->
+// 5 - Template Literals
+const userName = 'Igor'
+const age = 26
+
+console.log(`O nome do usuário é ${userName} e tem ${age} anos`)
+console.log('O nome do usuário é ' + userName + ' e ele tem ' + age + ' anos.')
+
+//--------------------------------------------------------------------------->
+// 6 - Destructuring
+const fruits = ['Maça', 'Laranja', 'Mamão']
+console.log(fruits)
+
+const [f1, f2, f3] = fruits
+console.log(f1)
+console.log(f2)
+console.log(f3)
+
+const productDetails = {
+    name: "Mouse",
+    price: 39.99,
+    category: "Pereféricos",
+    color: "Cinza"
+}
+
+const {name: productName, price, category: productCategory, color} = productDetails
+console.log(`O nome do produto é ${productName}, custa R$${price}, pertence a categoria ${productCategory}, e sua cor é ${color}`)
+
+//--------------------------------------------------------------------------->
+// 7 - Spread Operator
+const a1 = [1, 2, 3]
+const a2 = [4, 5, 6]
+const a3 = [...a1, ...a2]
+console.log(a3)
+const a4 = [0, ...a1, 4]
+console.log(a4)
+
+
+const carName = {name: 'Gol'}
+const carBrand = {brand: 'VW'}
+const otherInfos = {km: 100000, price: 49000}
+
+const car = {...carName, ...carBrand, ...otherInfos}
+console.log(car)
+
+//--------------------------------------------------------------------------->
+// 8 - Classes
+class Product {
+    constructor(name, price){
+        this.name = name
+        this.price = price
+    }
+
+    productWithDiscount(discount) {
+        return this.price * ((100 - discount) / 100)
+    }
+}
+
+const shirt = new Product("Camisa gola V", 20)
+console.log(shirt.name)
+console.log(shirt.price)
+
+console.log(shirt.productWithDiscount(10))
+console.log(shirt.productWithDiscount(50))
+
+const tenis = new Product ("Tenis verde", 120)
+console.log(tenis.name)
+console.log(tenis.productWithDiscount(25))
+//--------------------------------------------------------------------------->
+// 9 - Herança
+class ProductWithAttributes extends Product {
+    constructor(name, price, colors){
+        super(name, price)
+        this.colors = colors
+    }
+
+    showColors(){
+        console.log("As cores são: ")
+        this.colors.forEach((color) => {
+            console.log(color)
+        });
+    }
+}
+
+const hat = new ProductWithAttributes("Chapéu", 29.99, ["Preto", "Azul", "Verde"])
+
+console.log(hat.name)
+console.log(hat.productWithDiscount(30).toFixed(2))
+hat.showColors()
